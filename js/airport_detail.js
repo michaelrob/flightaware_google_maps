0754ef8ad02a48b39004c78a9710782f634d9ff2
Michael Robinson <seraph890@gmail.com>
2016-09-09 14:11:20 +1000

    updated airport detail js

--- js/airport_detail.js
+++ js/airport_detail.js
@@ -3,6 +3,13 @@ var airportContentString = '<div id="content">'+
     '</div>'+
     '<h1 id="firstHeading" class="firstHeading">'+ name +'</h1>'+
     '<div id="bodyContent">'+
+    '<h2>Address</h2>'+
     '<p></p>'+
+    '<h2>Telephone</h2>'+
     '<p></p>'+
+    '<h2>Fax</h2>'+
+    '<p></p>'+
+    '<h2>Email</h2>'+
+    '<p></p>'+
+    '<button>Read More</button>'+
     '</div>';
